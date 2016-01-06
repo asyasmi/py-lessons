70fcb025485f6dc2ed6f6163dd25768844454046
Anastasia Smirnova <anastasias@unity3d.com>
2016-01-07 00:25:10 +0200

    added GET from link, parsing link content

--- main.py
+++ main.py
@@ -2,16 +2,70 @@
 import getopt
 import sys 
 import os.path
+import requests
+import re
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+""""""""""""""""""""
+" Global variables "
+""""""""""""""""""""
+verbose = False
+
+
+def error(msg):
+	print("[ERROR] {0}".format(msg), file=sys.stderr)
+
+
+def debug(msg):
+	if verbose:
+		print("[DEBUG] {0}".format(msg), file=sys.stderr)
 
 
 def file_processing(file):
 	with open (file, "r") as f:
-		for line in f:
-			print(line, end='\n')
+		for url in f:
+			url = url.rstrip('\r\n')
+			if not url:
+				continue
+			if not re.match('^https?://', url):
+				error("Invalid link: '{0}'".format(url))
+				continue
+			html = None
+			try:
+				html = get_url_content(url)
+			except Exception as e:
+				error("Can't get content for {0}. Reason {1}.".format(url, e))
+				continue
+			#do some stuff and get all links
+			links_list = list()
+			soup = BeautifulSoup(html, "html.parser")
+			for link in soup.find_all("a"):
+				link = urljoin(url, link.get("href"))
+				debug("Link found: {0}".format(link))
+				links_list.append(link)
+
+
+def get_url_content(url, repeat=3):
+	counter = 0
+	debug("Processing url: {0}".format(url))
+	r = requests.get(url)
+	while (r.status_code != requests.codes.ok and counter < repeat):
+		r = requests.get(url)
+		counter += 1
+		debug(
+			"Repeat request: " + url + " at step " +
+			counter + " with reason " + r.status_code
+		)
+		#if (r.status_code != requests.codes.ok):
+	r.raise_for_status()
+	return r.text
+
 
 
 def usage():
 	print("Usage: {0} --file=~./data.txt".format(sys.argv[0]))
+
 
 
 def main():
@@ -24,12 +78,12 @@ def main():
 		usage()
 		sys.exit(1)
 	file = None
-	verbose = False
 	for o, a in opts:
 		if o in ("-h", "--help"):
 			usage()
 			sys.exit(0)
 		elif o in ("-v", "--verbose"):
+			global verbose
 			verbose = True
 		elif o in ("-f", "--file"):		
 			if a and os.path.isfile(a) and os.access(a, os.R_OK):
@@ -51,6 +105,6 @@ def main():
 	file_processing(file)
 
 
-
 if __name__ == "__main__":
     main()
+
